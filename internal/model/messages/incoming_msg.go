19aa6617daf977afb5dd0609deb8db099c98b0b4
ArikuWoW <91748313+ArikuWoW@users.noreply.github.com>
2025-04-12 18:04:29 +0300

    create "/add" command

--- internal/model/messages/incoming_msg.go
+++ internal/model/messages/incoming_msg.go
@@ -1,5 +1,10 @@
 package messages
 
+import (
+	"strconv"
+	"strings"
+)
+
 type MessageSender interface {
 	SendMessage(text string, userID int64) error
 }
@@ -17,10 +22,44 @@ type Message struct {
 	UserID int64
 }
 
-func (s *Model) IncomingMessage(msg Message) error {
-	if msg.Text == "/start" {
-		return s.tgClient.SendMessage("hello", msg.UserID)
+type Expense struct {
+	Amount int64
+	Group  string
+}
 
+var Expenses = []Expense{}
+
+func (s *Model) IncomingMessage(msg Message) error {
+	parts := strings.Fields(msg.Text)
+	if len(parts) == 0 {
+		return s.tgClient.SendMessage("Пустое сообщение", msg.UserID)
 	}
-	return s.tgClient.SendMessage("Не знаю эту команду", msg.UserID)
+
+	cmd := parts[0]
+
+	switch cmd {
+	case "/start":
+		return s.tgClient.SendMessage("Привет", msg.UserID)
+	case "/add":
+		if len(parts) < 3 {
+			return s.tgClient.SendMessage("Формат добавления расхода: /add <сумма> <товар>", msg.UserID)
+		}
+
+		amountStr := parts[1]
+		category := parts[2]
+		amount, err := strconv.Atoi(amountStr)
+		if err != nil {
+			return s.tgClient.SendMessage("Сумма должна быть числом", msg.UserID)
+		}
+
+		Expenses = append(Expenses, Expense{
+			Amount: int64(amount),
+			Group:  category,
+		})
+
+		return s.tgClient.SendMessage("Расход добавлен", msg.UserID)
+	default:
+		return s.tgClient.SendMessage("Не знаю эту команду", msg.UserID)
+	}
+
 }
